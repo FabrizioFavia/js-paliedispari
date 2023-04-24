7bf9f8c4b2a761ef196b8a1bafb88a420fbdd9bd
FabrizioFavia <personal.favia@gmail.com>
2023-04-24 12:17:36 +0200

    fix(js): fixed first func. & created palindromo()

--- js/main.js
+++ js/main.js
@@ -1,27 +1,46 @@
-function palindrome() {
+/**
+ * isPalindromo(word): funzione che che controlla se la parola è palindroma.
+ * word: argomento della funzione di tipo string.
+ * return: booleano (true se è palindromo, false se non lo è)
+ **/
 
+function isPalindromo(word) {
 
-    let word = prompt("inserisci una parola");
-    let reverseArray = [];
-    let reverseWord;
+    for (let i = 0; i < word.length; i++) {
 
-    for (let i = word.length - 1; i >= 0; i--) {
-        const letter = word[i];
+        /** 
+         * word[i]: iesimo carattere da controllare
+         * word.split('').reverse()[i]: iesimo elemento di word ribaltato.
+         * condizione: se l'iesimo carattere è diverso dall'iesimo carattere della parola ribaltata, ritorna false.
+         **/
 
-        reverseArray.push(letter);
-        reverseWord = reverseArray.join("");
+        if (word[i] != word.split('').reverse()[i]) {
+            return false;
+        }
     }
-
-    if (reverseWord == word) {
-        alert(`${reverseWord} è una parola palindroma`)
-
-    } else {
-        alert(`${word} non è una parola palindroma`)
-    }
-
-    return reverseWord;
-
+    return true;
 }
+
+
+
+function palindromo() {
+
+    let palindromeWord = prompt("Inserisci una parola da controllare")
+    
+    /* controlla se la parola è palindroma richiamando la funzione isPalindromo(word). */
+
+    if (isPalindromo(palindromeWord)) {
+        alert(`${palindromeWord} è una parola palindroma`);
+    } else {
+        alert(`${palindromeWord} non è una parola palindroma`);
+    }
+}
+
+
+
+
+
+
 
 
 
